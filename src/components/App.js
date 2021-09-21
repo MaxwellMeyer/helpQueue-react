28cff03ad9bacc0022d73948e09880e8ea69ca19
Maxwell Meyer <maxreadswell@gmail.com>
2021-09-21 11:23:50 -0700

    update app component to utilize router functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,22 @@
 import React from "react";
 import Header from "./Header";
 import TicketControl from "./TicketControl";
+import Signin from "./Signin";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <TicketControl />
-    </>
+      <Switch>
+        <Route path="/signin">
+          <Signin />
+        </Route>
+        <Route path="/">
+          <TicketControl />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
-
 export default App;
